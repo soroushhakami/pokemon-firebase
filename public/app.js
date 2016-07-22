c35961847670f6f0f30d4e0dcd0f13abb680ad7c
Soroush Hakami <soroush.hakami@comhem.com>
2016-07-22 10:29:45 +0200

    added auth

--- public/app.js
+++ public/app.js
@@ -1,7 +1,16 @@
-$( document ).ready(function() {
-    $('#add-pokemon').click(function(){
+$(document).ready(function () {
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            $('#loginStatus').text('user is logged in');
+        } else {
+            $('#loginStatus').text('user is logged out');
+        }
+    });
+
+    $('#add-pokemon').click(function () {
         var pokemonName = $('#pokemon').val();
-        if(pokemonName) {
+        if (pokemonName) {
             firebase.database().ref('pokemons').push({
                 name: pokemonName
             });
@@ -9,14 +18,44 @@ $( document ).ready(function() {
         }
     });
 
-    firebase.database().ref('pokemons').on('value', function(snapshot) {
+    firebase.database().ref('pokemons').on('value', function (snapshot) {
         $('#pokemon-list').empty();
 
         var pokemons = snapshot.val();
-        for(var pokemon in pokemons) {
-            $('#pokemon-list').append('<li>'+pokemons[pokemon].name+'</li>');
+        for (var pokemon in pokemons) {
+            $('#pokemon-list').append('<li>' + pokemons[pokemon].name + '</li>');
         }
-        console.log(snapshot.val());
     });
+
+    $('#login').click(function () {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(user);
+            // ...
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    });
+
+    $('#logout').click(function () {
+        firebase.auth().signOut().then(function () {
+            console.log('signed out');
+        }, function (error) {
+            // An error happened.
+        });
+    })
+
+
 });
 
